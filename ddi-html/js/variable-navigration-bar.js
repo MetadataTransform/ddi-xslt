5c470630a74cb712797aeb47aa9bb7606ecbb732
Olof Olsson <borsna@users.noreply.github.com>
2011-12-28 11:09:34 +0000

    moving some folders

---
+++ ddi-html/js/variable-navigration-bar.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+
+    $('<div id="variable-navigration"></div>').insertBefore('#study');
+
+});
+
+function pupulateVariableList(){
+    $('.variableScheme').each(function(index) {
+        
+    });
+  
+}
