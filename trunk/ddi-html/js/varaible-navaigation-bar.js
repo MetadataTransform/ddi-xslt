3923b662b0ea6fdcc88e057b7fc0d6cd43b34b94
borsna@gmail.com <borsna@gmail.com@ff5d64c2-029b-219a-7fee-58d066b0e445>
2011-12-29 15:53:54 +0000

    included basic variable navigation

--- trunk/ddi-html/js/varaible-navaigation-bar.js
+++ trunk/ddi-html/js/varaible-navaigation-bar.js
@@ -1,13 +1,24 @@
 $(document).ready(function(){
 
-    $('<div id="variable-navigration"></div>').insertBefore('#study');
+    $('<div id="variable-navigration">Navigation<div id="variable-list"></div></div>').insertBefore('#study');
     pupulateVariableList();
 });
 
 function pupulateVariableList(){
-    $('.variableScheme').each(function(index) {
-        var variableScheme = $(this);
-        console.log('variableScheme '+index);
+    var container = $("#variable-list");
+    $('.variableScheme .variables').each(function(index, variableScheme) {
+        console.log('variableScheme '+variableScheme);
+        $(variableScheme).children().each(function(i, variable){
+            console.log('variable '+$(variable).children('.label').text());
+            var name  = $(variable).children('.variableName').text();
+            var label =  $(variable).children('.label').text();
+            var href = $(variable).children('a').attr('name')
+            
+            $(container).append('<li><a href="#'+href+'"><strong class="variableName">'+name+'</strong><span class="variablel-label">'+label+'</span></a></li>');
+            
+        });
     });
+    
+    $('#study').css('margin-left', $('#variable-navigration').width());
   
 }
