6e3c8949199903eca56b121ce7ab988be95ede65
Olof Olsson <borsna@users.noreply.github.com>
2011-12-29 16:08:27 +0000

    added filter for variables

--- ddi-html/js/varaible-navaigation-bar.js
+++ ddi-html/js/varaible-navaigation-bar.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 
-    $('<div id="variable-navigration">Navigation<div id="variable-list"></div></div>').insertBefore('#study');
+    $('<div id="variable-navigration"><input class="filter" name="livefilter" type="text" placeholder="filter" /><div id="variable-list-wrapper"><ul id="variable-list"></ul></div></div>').insertBefore('#study');
     pupulateVariableList();
 });
 
@@ -20,5 +20,50 @@ function pupulateVariableList(){
     });
     
     $('#study').css('margin-left', $('#variable-navigration').width());
-  
+    $('#variable-list-wrapper').liveFilter('ul');
 }
+
+/***** [plugins] ****/
+
+/***********************************************************/
+/*                    LiveFilter Plugin                    */
+/*                      Version: 1.1                       */
+/*                      Mike Merritt                       */
+/*             	    Updated: Feb 2nd, 2010                 */
+/***********************************************************/
+
+(function($){  
+	$.fn.liveFilter = function (wrapper) {
+		
+		// Grabs the id of the element containing the filter
+		var wrap = '#' + $(this).attr('id');
+		
+		// Make sure we're looking in the correct sub-element
+		if (wrapper == 'table') {
+			var item = 'tr';
+		} else {
+			var item = 'li';
+		}
+		
+		// Listen for the value of the input to change
+		$('input.filter').keyup(function() {
+			var filter = $(this).val();
+			
+			// Hide all the items and then show only the ones matching the filter
+			$(wrap + ' ' + wrapper + ' ' + item).hide();
+			
+			if (wrapper == 'table') {
+				$(wrap + ' ' + wrapper + ' ' + 'tr.header').show();
+			}
+			$(wrap + ' ' + wrapper + ' ' + item + ':Contains("' + filter + '")').show();
+			
+		});
+		
+		// Custom expression for case insensitive contains()
+		jQuery.expr[':'].Contains = function(a,i,m){
+		    return jQuery(a).text().toUpperCase().indexOf(m[3].toUpperCase())>=0;
+		};
+
+	}
+
+})(jQuery);
