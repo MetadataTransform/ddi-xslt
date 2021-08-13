6d54c88cdb267c82c0266b871e866141d0a737da
alireza1111 <alireza.davoudian@snd.gu.se>
2021-08-13 13:47:03 +0200

    trans script added

---
+++ dev/run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "starting run.sh"
+
+SAXON="/utils/saxon.jar"
+
+echo "Dev script to auto-transform XSLT while editing the XSLT"
+echo "start watch for modifications in $XSLT"
+echo "result will be shown in $XML.result.xml"
+while inotifywait -q -e modify $XSLT; do
+    java -jar $SAXON $XML $XSLT -o:$XML.result.xml
+done
