3618a2296c099b11463c4bc182ff8921ddec43cd
Olof Olsson <olof.olsson@snd.gu.se>
2022-01-24 16:47:14 +0100

    adding cmv validation (optional)

--- dev/run.sh
+++ dev/run.sh
@@ -3,10 +3,15 @@
 echo "starting run.sh"
 
 SAXON="/utils/saxon.jar"
+CMV="/utils/cmv-cmd.jar"
 
 echo "Dev script to auto-transform XSLT while editing the XSLT"
 echo "start watch for modifications in $XSLT"
 echo "result will be shown in $XML.result.xml"
 while inotifywait -q -e modify $XSLT; do
     java -jar $SAXON $XML $XSLT -o:$XML.result.xml
+
+    if [[ ! -z "$PROFILE" ]]; then
+        java -jar $CMV $XML.result.xml $PROFILE
+    fi
 done
